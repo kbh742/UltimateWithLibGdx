a600641b7b5308a67ea77b265495963c6a09d5ce
fkerr0890 <numbernerd2@gmail.com>
2016-09-22 12:56:23 -0700

    background

--- core/src/com/example/kholt6406/frisbee_app/States/MenuState.java
+++ core/src/com/example/kholt6406/frisbee_app/States/MenuState.java
@@ -3,15 +3,12 @@ package com.example.kholt6406.frisbee_app.States;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-/**
- * Created by kholt6406 on 9/20/2016.
- */
 public class MenuState extends State{
     private Texture background;
-
+    private Texture playBtn;
     public MenuState(GameStateManager gsm) {
         super(gsm);
-        background = new Texture("Ultimate.png");
+        background = new Texture("Ultimate.jpg");
     }
 
     @Override
@@ -27,7 +24,8 @@ public class MenuState extends State{
     @Override
     public void render(SpriteBatch sb) {
         sb.begin();
-        sb.draw(background, 0,0);
+        sb.draw(background, 0,0, 480,800);
+        sb.end();
 
     }
 }
