2e647550cfdf9b9a58a6e6251b54455f5fdbe03c
kbh742 <holtkeaton@gmail.com>
2016-10-10 12:57:51 -0700

    touchpad is now functioning!

--- core/src/com/example/kholt6406/frisbee_app/States/PlayState.java
+++ core/src/com/example/kholt6406/frisbee_app/States/PlayState.java
@@ -3,6 +3,8 @@ package com.example.kholt6406.frisbee_app.States;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
@@ -10,6 +12,8 @@ import com.example.kholt6406.frisbee_app.sprites.Player;
 
 public class PlayState extends State {
     private Player player;
+    private Stage stage;
+    private SpriteBatch sb;
     private Texture background;
     private Touchpad touchpad;
     private Touchpad.TouchpadStyle touchpadStyle;
@@ -17,17 +21,22 @@ public class PlayState extends State {
     private Drawable touchBackground;
     private Drawable touchKnob;
     private Texture scoreboard;
+    private EventListener listener;
+
     public PlayState(GameStateManager gsm) {
         super(gsm);
         player=new Player(50,100);
         cam.setToOrtho(false,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
         background = new Texture("field_background.png");
         scoreboard=new Texture("scoreboard.png");
+        //Texture joystickKnob = new Texture("joystick_base.png");
+        //joystickKnob.
+
         touchpadSkin = new Skin();
         //Set background image
-        touchpadSkin.add("touchBackground", new Texture("touchBackground.png"));
+        touchpadSkin.add("touchBackground", new Texture("joystick_base.png"));
         //Set knob image
-        touchpadSkin.add("touchKnob", new Texture("touchKnob.png"));
+        touchpadSkin.add("touchKnob", new Texture("joystick_64.png"));
         //Create TouchPad Style
         touchpadStyle = new Touchpad.TouchpadStyle();
         //Create Drawable's from TouchPad skin
@@ -39,7 +48,11 @@ public class PlayState extends State {
         //Create new TouchPad with the created style
         touchpad = new Touchpad(10, touchpadStyle);
         //setBounds(x,y,width,height)
-        touchpad.setBounds(15, 15, 200, 200);
+        touchpad.setBounds(15, 15, 400, 400);
+
+        stage = new Stage();
+        stage.addActor(touchpad);
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
@@ -56,13 +69,17 @@ public class PlayState extends State {
     @Override
     protected void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
+        player.setX(player.getPosition().x + touchpad.getKnobPercentX()*player.getVelocity());
+        player.setY(player.getPosition().y + touchpad.getKnobPercentY()*player.getVelocity());
         sb.begin();
         sb.draw(background, 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         sb.draw(player.getTexture(),player.getPosition().x,player.getPosition().y);
         sb.draw(scoreboard,(Gdx.graphics.getWidth()/2)-(scoreboard.getWidth()*3/2),Gdx.graphics.getHeight()-scoreboard.getHeight()*3,scoreboard.getWidth()*3,scoreboard.getHeight()*3);
-        //sb.draw(scoreboard,(Gdx.graphics.getWidth()/2)-(scoreboard.getWidth()),0);
-        //touchpad.draw(sb, 0);
+        touchpad.draw(sb,1);
         sb.end();
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 
     @Override
--- core/src/com/example/kholt6406/frisbee_app/sprites/Player.java
+++ core/src/com/example/kholt6406/frisbee_app/sprites/Player.java
@@ -7,28 +7,34 @@ import com.badlogic.gdx.math.Vector3;
 public class Player {
     private static final int ACCELERATION=15;
     private Vector3 position;
-    private Vector3 velocity;
+    private float velocity;
     private Texture player;
+    float x = 0;
+    float y = 0;
 
     public Player(int x, int y){
         position=new Vector3(50,50,0);
-        velocity=new Vector3(30,0,0);
+        velocity= 10;
         player=new Texture("idle.png");
     }
 
-    public void setVelocity(){
-        //velocity
-    }
+    public void setX(float xPos) { x = xPos; }
+
+    public void setY(float yPos) { y = yPos; }
+
+    public void setVelocity() {}
 
     public void update(float dt){
-        velocity.scl(dt);
-        position.add(velocity.x,0,0);
-        velocity.scl(1/dt);
+        //velocity.scl(dt);
+        position.set(x,y,0);
+        //velocity.scl(1/dt);
     }
 
     public Vector3 getPosition() {
         return position;
     }
+
+    public float getVelocity() { return velocity; }
 
     public Texture getTexture() {
         return player;
